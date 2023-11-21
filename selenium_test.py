7e1be33740adc7794c24869ed3d9b8b430225677
Jay Reddy <jayreddy@gmail.com>
2023-11-20 23:23:57 -0800

    refactored time.sleep() to webdriverwait

--- selenium_test.py
+++ selenium_test.py
@@ -2,43 +2,34 @@ import os
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv()
 
-# Retrieve credentials from environment variables
 username = os.environ.get('LINKEDIN_USER')
 password = os.environ.get('LINKEDIN_PW')
 linkedin_jobs_url = os.environ.get('LINKEDIN_JOBS_URL')
 
-# Initialize the WebDriver (Chrome in this case)
 driver = webdriver.Chrome()
 
-# Open LinkedIn
 driver.get("https://www.linkedin.com")
 
-# Wait for the login page to load
-time.sleep(2)
+wait = WebDriverWait(driver, 10)
 
-# Find the username field and enter the username
-username_field = driver.find_element(By.ID, 'session_key')
+username_field = wait.until(
+    EC.visibility_of_element_located((By.ID, 'session_key')))
 username_field.send_keys(username)
 
-# Find the password field and enter the password
-password_field = driver.find_element(By.ID, 'session_password')
+password_field = wait.until(
+    EC.visibility_of_element_located((By.ID, 'session_password')))
 password_field.send_keys(password)
 
-# Find the sign-in button and click it
-sign_in_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+sign_in_button = wait.until(EC.element_to_be_clickable(
+    (By.XPATH, '//button[@type="submit"]')))
 sign_in_button.click()
 
-# Wait for the main page to load
-time.sleep(5)
-
-# Navigate to the 'Jobs' section
 driver.get(linkedin_jobs_url)
-# Add additional code here to interact with the jobs page
 
-# Close the browser after a delay
-time.sleep(5)
+
 driver.quit()
